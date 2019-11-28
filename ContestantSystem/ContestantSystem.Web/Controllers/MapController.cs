f79f0963197a33534d9647579854e489945d24e8
Sagar Gupta <gpt16sagar@hotmail.com>
2019-11-28 13:56:41 +0545

    Rendering Contestant according to district in Map , Done!!

--- ContestantSystem/ContestantSystem.Web/Controllers/MapController.cs
+++ ContestantSystem/ContestantSystem.Web/Controllers/MapController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContestantSystem.Service.Contestant;
+using ContestantSystem.Service.Districts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,28 @@ namespace ContestantSystem.Web.Controllers
 {
     public class MapController : Controller
     {
+       
+        private readonly IContestantService _ContestantService;
+
+        public MapController()
+        {
+           
+            _ContestantService = new ContestantService();
+        }
+
+
         // GET: Map
         public ActionResult Index()
         {
+            LoadContestantDensity();
             return View();
+        }
+
+
+        private void LoadContestantDensity()
+        {
+            var list = _ContestantService.GetAllContestants().Where(x => x.IsActive == true).ToList();
+            ViewBag.ContestantList = list;
         }
     }
 }
